8d0e496ac8f1aa1f7c8d1c50f6913ec4cc660e12
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:19:19 -0800

    vicky: Setup 6GB Dalvik heap configuration
    Change-Id: Ia2b826c95643f935878335c2434deadf7f8401c4

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Boot Control HAL
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
