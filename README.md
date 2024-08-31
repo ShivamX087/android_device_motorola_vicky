b16ce82007a222e607553a9264819727f1276240
claxten10 <claxten10@gmail.com>
2024-08-31 06:34:54 +0200

    vicky: Initial skeleton tree
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: ShivamX087 <shivamx976@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# AOSP Device Tree for the Motorola G72 4G (vicky)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_vicky.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/motorola/vicky/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_vicky
+PRODUCT_DEVICE := vicky
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto G72
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="vicky_g_sys-user 13 T2SVS33M.68-21-8-14 d6e5ed release-keys" \
+    BuildFingerprint=motorola/vicky_g_sys/vicky:13/T2SVS33M.68-21-8-14/d6e5ed:user/release-keys
