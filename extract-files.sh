5b055f801c076cb9b0c56616cd9d9e8913b1eeb1
Giovanni Ricca <giovanniricca@proton.me>
2025-03-31 11:20:25 +0530

    vicky: Link libsink.so to libaudioclient_shim
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup {
+    case "$1" in
+        system_ext/lib64/libsink.so)
+           "${PATCHELF}" --add-needed "libaudioclient_shim.so" "$2"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
