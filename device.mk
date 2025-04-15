8e4fb3b577351dc937fc8a336c30d16a351ae686
ShivamX087 <shivamx976@gmail.com>
2025-04-15 19:46:18 +0530

    vicky: Configure keymaster stack
    Signed-off-by: ShivamX087 <shivamx976@gmail.com>

--- device.mk
+++ device.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
     init.sensor_2_0.rc \
     ueventd.mt6789.rc
 
+# Keymaster
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
+
 # Modules
 PRODUCT_PACKAGES += \
     init.insmod.sh \
